29a2da795ff096a80b4ae62d854f425cc2f1fa11
Hristo Pandurski <hristopandurski94@gmail.com>
2018-02-23 19:41:20 +0200

    Styling the sidebar

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { slideFromRight } from '../../animations';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+  animations: [slideFromRight()]
 })
 export class SidebarComponent implements OnInit {
 
